c89076f293a07f24149e99d819b1b05d4aca9df5
‘mxg133’ <396330646@qq.com>
2020-11-21 17:12:35 +0800

    2020-11-21

--- Spring_SpringMVC/src/com/DanceBytes/bean/User.java
+++ Spring_SpringMVC/src/com/DanceBytes/bean/User.java
@@ -1,13 +1,21 @@
 package com.DanceBytes.bean;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @author 孟享广
  * @date 2020-11-21 4:08 下午
  * @description
  */
+
+@Component
 public class User {
     private Integer id;
     private String name;
+
+    public User() {
+        System.out.println("默认构造.....");
+    }
 
     public void setId(Integer id) {
         this.id = id;
---
+++ Spring_SpringMVC/src/com/DanceBytes/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.DanceBytes.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author 孟享广
+ * @date 2020-11-21 4:26 下午
+ * @description
+ */
+
+@Controller
+public class TestController {
+}
